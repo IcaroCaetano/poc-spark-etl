d8fe8d7941f34be345df57df32474360ee43d8b9
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-07 14:45:40 -0300

    Update ETLJob.scala

--- src/main/scala/com/projecticaro/etl/ETLJob.scala
+++ src/main/scala/com/projecticaro/etl/ETLJob.scala
@@ -1,1 +1,25 @@
+package com.projecticaro.etl
 
+import org.apache.spark.sql.SparkSession
+
+object ETLJob {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("ETLProjectIcaroScala")
+      .master("local[*]")
+      .config("spark.sql.shuffle.partitions", "2")
+      .getOrCreate()
+
+    // Extraction: read CSV
+    val df = spark.read.option("header", true).csv("data/input/sample_data.csv")
+
+    // Transformation: remove nulls and duplicates
+    val dfClean = df.na.drop().dropDuplicates()
+
+    // Load: save to Parquet
+    dfClean.write.mode("overwrite").parquet("data/output/cleaned_data_scala.parquet")
+
+    println("✅ ETL completed successfully (Scala)!")
+    spark.stop()
+  }
+}
